17f75a381a551cff567d81a2f95437b30c868d94
Rahul Rajput <rahulk@techaheadcorp.com>
2025-04-22 08:39:06 +0530

    19 problem solved.

---
+++ Easy/Arrays/1572. Matrix Diagonal Sum.swift
@@ -0,0 +1,58 @@
+/*
+Given a square matrix mat, return the sum of the matrix diagonals.
+
+Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+
+
+Example 1:
+
+
+Input: mat = [[1,2,3],
+              [4,5,6],
+              [7,8,9]]
+Output: 25
+Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+Notice that element mat[1][1] = 5 is counted only once.
+Example 2:
+
+Input: mat = [[1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1]]
+Output: 8
+Example 3:
+
+Input: mat = [[5]]
+Output: 5
+
+
+Constraints:
+
+n == mat.length == mat[i].length
+1 <= n <= 100
+1 <= mat[i][j] <= 100
+*/
+
+class Solution {
+  func diagonalSum(_ mat: [[Int]]) -> Int {
+    let n = mat.count
+    var i = 0
+    var result = 0
+    while i < n {
+      print(mat[i][i], mat[i][(n - 1) - i])
+      result = mat[i][i] + result
+      result = mat[i][(n - 1) - i] + result
+      i += 1
+    }
+    if (n % 2) != 0 {
+      let mid = n / 2
+      result = result - mat[mid][mid]
+    }
+    return result
+  }
+}
+
+var mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+let result = Solution().diagonalSum(mat)
+print(result)
